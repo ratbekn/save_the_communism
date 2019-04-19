2c08f564f7d1310f0f5e777d578cf26f9aa89f68
devmentality <nikita.volnov.1999@mail.ru>
2019-04-20 00:58:23 +0500

    collision resolver

---
+++ collision.py
@@ -0,0 +1,27 @@
+import math
+from collections import defaultdict
+
+
+class CollisionsResolver:
+    @staticmethod
+    def resolve_collisions(game_objects):
+        collisions = CollisionsResolver._get_collisions_dict(game_objects)
+        for object in collisions.keys():
+            object.handle_collisions(collisions[object])
+
+    @staticmethod
+    def _are_collided(first, second):
+        distance = math.sqrt((first.x - second.x) ** 2 + (first.y - second.y) ** 2)
+        return distance <= first.radius + second.radius
+
+    @staticmethod
+    def _get_collisions_dict(game_objects):
+        collisions = defaultdict(list)
+
+        for i in range(len(game_objects)):
+            for j in range(i + 1, len(game_objects)):
+
+                first, second = game_objects[i], game_objects[j]
+                if CollisionsResolver._are_collided(first, second):
+                    collisions[first].append(second)
+                    collisions[second].append(first)
