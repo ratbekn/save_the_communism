9c444c24a5713c23f9b51481042c585cc14cdbb9
GH2OOST <38152065+GH2OOST@users.noreply.github.com>
2019-04-19 23:04:04 +0500

    game_object base class added

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from game import Game
 
 
 def main():
-    Game('save_the_communism', 800, 600, "images/background.png", 60).run()
+    Game('Save The Communism', 800, 600, "images/background.png", 60).run()
+
 
 
 if __name__ == '__main__':
